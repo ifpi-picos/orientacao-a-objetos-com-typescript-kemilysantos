2cc8ec385948b7db055d49dc6daebd727c2d1cf9
kemilysantos <kemilysantos779@gmail.com>
2021-08-17 17:05:50 -0300

    Correção nos testes

--- teste/funcionario.test.ts
+++ teste/funcionario.test.ts
@@ -6,7 +6,7 @@ describe('funcionario', () => {
     const NOME_FUNCIONARIO = 'Carlos'
     const NOME_FUNCIONARIO1 = 'Michel'
     const NOME_FUNCIONARIO2 = 'Edivan'
-    const SALA_FUNCIONARIO = 1500
+    const SALA_FUNCIONARIO = 2000
     const CNPJ_FUNCIONARIO = '6587864944548'
     const CPF_FUNCIONARIO = '6468651487818'
 
@@ -15,17 +15,17 @@ describe('funcionario', () => {
         expect(funcionario.getNome()).toBe(NOME_FUNCIONARIO)
         
     })
-    it('adicionar pf com um salario de 1350', () => {
+    it('adicionar pf com um salario de 1000', () => {
         const funcionario1 = new PF(NOME_FUNCIONARIO1, SALA_FUNCIONARIO, CPF_FUNCIONARIO)
-        expect(funcionario1.getNome()) .toBe(NOME_FUNCIONARIO1)
-        expect(funcionario1.getBeneficios() .length) .toBe(3)
-        expect(funcionario1.getSalarioLiquido()) .toBe(1350)
+        expect(funcionario1.getNome()).toBe(NOME_FUNCIONARIO1)
+        expect(funcionario1.getBeneficios() .length).toBe(3)
+        expect(funcionario1.getSalario()).toBe(SALA_FUNCIONARIO)
     })
     
     it('pj com o salario de 1425', () => {
         const funcionario2 = new PJ(NOME_FUNCIONARIO2, SALA_FUNCIONARIO, CNPJ_FUNCIONARIO)
         expect(funcionario2.getNome()).toBe(NOME_FUNCIONARIO2)
-        expect(funcionario2.getBeneficios() .length) .toBe(1)
-        expect(funcionario2.getSalarioLiquido()) .toBe(1425)
+        expect(funcionario2.getBeneficios() .length).toBe(1)
+        expect(funcionario2.getSalario()).toBe(SALA_FUNCIONARIO)
     })
 })
